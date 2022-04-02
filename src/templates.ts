ca82b692d24bdc2fe3ba0074b7c6f9eb6dd8391d
yan3321 <github@yan3321.com>
2022-04-02 11:21:00 +0800

    fix: json loading fails on linux

--- src/templates.ts
+++ src/templates.ts
@@ -4,10 +4,12 @@ const { loadImage } = pkg;
 import { NametagTemplate } from "./nametag.js";
 import { getAssetFromId } from "./roblox-image-utils.js";
 
-import templates from "../resources/templates.json";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const templates = require("../resources/templates.json");
 
 const loadedTemplates: NametagTemplate[] = await Promise.all(
-  templates.map(async (item) => {
+  templates.map(async (item: any) => {
     const data = item;
     const assets = {
       canvasImage: await loadImage(`./resources/templates/${item.imagePath}`),
